71663af80436ae21941363754265294c64517933
YoshThePinetree <p349532y@mail.kyutech.jp>
2020-05-29 18:10:02 +0900

    First commit

--- Main.py
+++ Main.py
@@ -2,10 +2,11 @@ import numpy as np
 import TestFunctions
 
 
-test = TestFunctions.Functions("Rastrigin")
-# test = TestFunctions.Functions("Ackley")
+# test = TestFunctions.Functions("Rastrigin")
+test = TestFunctions.Functions("Ackley")
 # x = np.array([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
-x = np.array([[0.0, 0.0, 0.0], [-5.12, -5.12, -0.3], [0.0, 1.0, 2.0], [0.0, 0.0, 0.0]])
+# x = np.array([[0.0, 0.0, 0.0], [-5.12, -5.12, -0.3], [0.0, 1.0, 2.0], [0.0, 0.0, 0.0]])
+x = np.array([[0.0, 0.0],[-5.8, -100]])
 x = test.calc(x)
 print(x)
 
--- TestFunctions.py
+++ TestFunctions.py
@@ -7,7 +7,7 @@ class Functions:
         self.Fname = name
 
     def calc(self, x):
-        if self.Fname == "Rastrigin":
+        if self.Fname == "Rastrigin":   # allows any number of dimensions
             # Limitation of search domain
             x = np.where(x <= -5.12, -5.12, x)
             x = np.where(x >= 5.12, 5.12, x)
@@ -20,9 +20,18 @@ class Functions:
                 f[i] = f[i] + A * np.shape(x)[1]
 
             return f
-        if self.Fname == "Ackley":
-            y = x + 100
-            return y
+        if self.Fname == "Ackley":  # 2 dimensions only
+            # Limitation of search domain
+            x[:,0] = np.where(x[:,0] <= -5, -5, x[:,0])
+            x[:,1] = np.where(x[:,1] >= 5, 5, x[:,1])
+
+            f = np.zeros(np.shape(x)[0])
+            for i in range(np.shape(x)[0]):
+                f[i] = -20 * math.exp(-0.2 * math.sqrt(0.5 * (x[i,0]**2 + x[i,1]**2))) - \
+                       math.exp(0.5 * (math.cos(2*math.pi * x[i,0]) + math.cos(2*math.pi * x[i,1]))) + \
+                       math.exp(1) + 20
+
+            return f
         else:
             y = x + 2
             return y
