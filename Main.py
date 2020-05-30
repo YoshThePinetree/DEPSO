7d977954677b838ae8136a41c91a58154344c174
YoshThePinetree <p349532y@mail.kyutech.jp>
2020-05-30 22:26:07 +0900

    Built the PSO foundation

--- Main.py
+++ Main.py
@@ -1,12 +1,65 @@
-import numpy as np
 import TestFunctions
+import numpy as np
+import random
 
 
+print("DEPSO Start")
+# ========== #
+# Initiation #
+# ========== #
+### Parameters
 # test = TestFunctions.Functions("Rastrigin")
-test = TestFunctions.Functions("Ackley")
-# x = np.array([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
-# x = np.array([[0.0, 0.0, 0.0], [-5.12, -5.12, -0.3], [0.0, 1.0, 2.0], [0.0, 0.0, 0.0]])
-x = np.array([[0.0, 0.0],[-5.8, -100]])
-x = test.calc(x)
-print(x)
+# test = TestFunctions.Functions("Ackley")
+# test = TestFunctions.Functions("Rosenbrock")
+test = TestFunctions.Functions("Griewank")
+tmax = 10   # the number of maximum trial
+imax = 200  # the number of maximum iteration
+npop = 20   # the number of particles
+ndim = 5    # the number of dimensions
+w = 0.4     # the inertia weight
+c1 = 2      # weight for the personal best
+c2 = 2      # weight for the global best
+
+### Memory Allocation
+F = np.zeros((tmax,imax))
+Xarc = np.zeros((tmax,imax,ndim))
+random.seed(0)
+
+# ========= #
+# Main Loop #
+# ========= #
+for t in range(tmax):
+    X = test.initialize(npop, ndim)  # the particle position
+    V = test.initialize(npop, ndim)  # the particle velocity
+    X1 = np.zeros((npop, ndim))
+    V1 = np.zeros((npop, ndim))
+    Xp = X                           # personal best individuals
+    f = test.calc(X)                 # first fitness
+    f1 = np.zeros_like(f)
+    Xg = X[np.argmin(f),:]           # the personal best
+    fg = np.min(f)
+
+    ### Loop of a trial
+    for ite in range(imax):
+        for i in range(npop):
+            for j in range(ndim):
+                V1[i,j] = w*V[i,j] + c1*random.random()*(Xp[i,j] - X[i,j]) + \
+                          c2*random.random()*(Xg[j] - X[i,j])
+        X1 = X + V1  # position update
+        f1 = test.calc(X1)  # updated fitness
+
+        for i in range(npop):   # personal best update
+            if f1[i] < f[i]:
+                f[i] = f1[i]
+                Xp[i,:] = X1[i,:]
+        fg1 = np.min(f1)        # global best update
+        if fg1 < fg:
+            fg = fg1
+            Xg = X1[np.argmin(f1),:]
+
+        X = X1
+        V = V1
+        F[t,ite] = fg
+        Xarc[t,ite,:] = Xg
+        print("OF ",t,"-",ite," :",fg)
 
--- TestFunctions.py
+++ TestFunctions.py
@@ -1,17 +1,37 @@
 import numpy as np
 import math
+import random
 
 
 class Functions:
     def __init__(self, name):
         self.Fname = name
+        if self.Fname == "Rastrigin":
+            self.llim = -5.12
+            self.ulim = 5.12
+        if self.Fname == "Ackley":
+            self.llim = -5
+            self.ulim = 5
+        if self.Fname == "Rosenbrock":
+            self.llim = -10
+            self.ulim = 10
+        if self.Fname == "Griewank":
+            self.llim = -600
+            self.ulim = 600
+
+    def initialize(self, npop, ndim):
+        y = np.zeros((npop, ndim))
+        for i in range(npop):
+            for j in range(ndim):
+                y[i,j] = random.uniform(self.llim,self.ulim)
+        return y
 
     def calc(self, x):
-        if self.Fname == "Rastrigin":   # allows any number of dimensions
-            # Limitation of search domain
-            x = np.where(x <= -5.12, -5.12, x)
-            x = np.where(x >= 5.12, 5.12, x)
+        # Limitation of search domain
+        x = np.where(x <= self.llim, self.llim, x)
+        x = np.where(x >= self.ulim, self.ulim, x)
 
+        if self.Fname == "Rastrigin":   # allows any number of dimensions
             A = 10
             f = np.zeros(np.shape(x)[0])
             for i in range(np.shape(x)[0]):
@@ -32,6 +52,19 @@ class Functions:
                        math.exp(1) + 20
 
             return f
-        else:
-            y = x + 2
-            return y
+        if self.Fname == "Rosenbrock":  # allows any number of dimensions
+            f = np.zeros(np.shape(x)[0])
+            for i in range(np.shape(x)[0]):
+                for j in range(np.shape(x)[1]-1):
+                    f[i] = f[i] + (100*(x[i,j+1] - x[i,j]**2)**2 + (1 - x[i,j])**2)
+            return f
+
+        if self.Fname == "Griewank":  # allows any number of dimensions
+            f1 = np.zeros((np.shape(x)[0]))
+            f2 = np.ones((np.shape(x)[0]))
+            for i in range(np.shape(x)[0]):
+                for j in range(np.shape(x)[1]):
+                    f1[i] = f1[i] + (x[i,j]**2 / 4000)
+                    f2[i] = f2[i] * (math.cos(x[i,j] / math.sqrt(i+1)))
+            f = f1 - f2 + 1
+            return f
